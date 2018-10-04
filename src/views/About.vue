263a5b5f715c93e9828754347cc0d64d53b5ace3
caixw <xnotepad@gmail.com>
2018-10-04 22:48:10 +0800

    添加缺失翻译文件和 vuetify-types.ts

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>部分页面借鉴了 <a href="https://darosh.github.io/oax">https://darosh.github.io/oax/</a>的内容</div>
   </div>
 </template>
---
+++ src/vuetify-types.ts
@@ -0,0 +1,17 @@
+// Copyright 2018 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// 定义了部分 vuetify 中需要用到的接口类型
+
+export interface DataTableHeadersItem {
+  locale: string // 本地化的值
+  localeTip?: string // tip 的本地化 ID
+
+  // 以下为 vuetify 中需要提供的信息
+  value: string  // 对应数据中的显示字段名
+  text?: string  // 显示在 header 中的内容
+  align?: 'left' | 'center' | 'right'
+  sortable?: boolean
+  width?: string
+}
