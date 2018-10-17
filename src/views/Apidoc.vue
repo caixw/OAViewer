ab9e717bb5620d42be311ec2ddb5b833448087fb
caixw <xnotepad@gmail.com>
2018-10-18 00:26:31 +0800

    添加 Apidoc.vue 页面

---
+++ src/views/Apidoc.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Headers extends Vue {
+  private data: Object = {}
+
+  public load(url: string) {
+    fetch(url).then(resp => {
+      // TODO
+    }).catch(reason => {
+      console.error('加载文档内容失败：', reason)
+    })
+  }
+}
+</script>
