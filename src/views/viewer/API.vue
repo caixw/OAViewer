2b85c118ee69e544e674cfeb16ae57102411bb78
caixw <xnotepad@gmail.com>
2018-11-12 23:21:52 +0800

    调整界面

--- src/views/viewer/API.vue
+++ src/views/viewer/API.vue
@@ -3,8 +3,7 @@
     <v-card>
         <v-toolbar flat>
           <v-toolbar-title>
-            <v-chip disabled label>{{api.method}}</v-chip>
-            {{api.path}}
+            {{api.summary}}
           </v-toolbar-title>
           <v-spacer />
           <v-btn icon @click="visible=false">
@@ -13,17 +12,45 @@
         </v-toolbar>
 
       <v-card-text>
-        // tesxt
+        <v-chip disabled label small>{{api.method}}</v-chip>
+        {{api.path}}
+
+        <vue-markdown v-if="api.description" :source="api.description" />
       </v-card-text>
+
+      <v-expansion-panel style="outline:none">
+        <v-expansion-panel-content>
+          <v-subheader class="pl-0 pr-0" slot="header" v-t="'viewer.api.response'" />
+          <v-card>
+            <v-card-text>
+              ddd
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+
+        <v-expansion-panel-content>
+          <v-subheader class="pl-0 pr-0" slot="header" v-t="'viewer.api.request'" />
+          <v-card>
+            <v-card-text>
+              //test
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
     </v-card>
   </v-dialog>
 </template>
 
 <script lang="ts">
+import VueMarkdown from 'vue-markdown'
 import { Component, Vue, Prop } from 'vue-property-decorator'
 import { API, api } from './types'
 
-@Component
+@Component({
+  components: {
+  VueMarkdown
+  }
+  })
 export default class VApi extends Vue {
   api: API = api
   visible: boolean = false
