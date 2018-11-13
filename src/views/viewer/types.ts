761a7291e68bc775774a1f5cc03376131cc56e59
caixw <xnotepad@gmail.com>
2018-11-13 20:45:33 +0800

    统一格式

--- src/views/viewer/types.ts
+++ src/views/viewer/types.ts
@@ -27,37 +27,37 @@ export interface Callback {
 }
 
 export interface Request {
-	mimetype: string
-	type: Schema
-	headers?: Header[]
-	examples?: Example
+  mimetype: string
+  type: Schema
+  headers?: Header[]
+  examples?: Example
 }
 
 export interface Response {
   status: number
-	mimetype: string
-	type: Schema
-	headers?: Header[]
-	examples?: Example
+  mimetype: string
+  type: Schema
+  headers?: Header[]
+  examples?: Example
 }
 
 export interface Param {
-	name: string
-	type: Schema
-	summary: string
-	optional?: boolean
+  name: string
+  type: Schema
+  summary: string
+  optional?: boolean
 }
 
 export interface Header{
-	name: string
-	summary: string
-	optional?: boolean
+  name: string
+  summary: string
+  optional?: boolean
 }
 
 export interface Example{
-	mimetype: string
-	summary: string
-	value: string
+  mimetype: string
+  summary: string
+  value: string
 }
 
 export interface Schema {
@@ -72,5 +72,5 @@ export const api = {
   queries: [],
   summary: 'test',
   requests: [],
-  responses: [],
+  responses: []
 }
