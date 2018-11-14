dd8ce7d454829668ca3cf512e4df5f5a00360c7b
caixw <xnotepad@gmail.com>
2018-11-14 22:23:20 +0800

    更新界面

--- src/views/viewer/Response.vue
+++ src/views/viewer/Response.vue
@@ -1,14 +1,76 @@
 <template>
-  <div></div>
+  <div>
+    <h3 class="subheading" v-if="response.mimetype != '*'">{{response.mimetype}}</h3>
+
+    <v-data-table v-if="hasHeaders"
+    :headers="paramsHeaders"
+    :items="response.headers"
+    :hide-actions="true">
+      <template slot="headerCell" slot-scope="props">
+        <span v-t="props.header.locale" />
+      </template>
+
+      <template slot="items" slot-scope="props">
+        <th class="text-xs-left">{{props.item.name}}</th>
+        <td><v-icon>{{checkbox(!props.item.optional)}}</v-icon></td>
+        <td v-html="props.item.summary" />
+      </template>
+    </v-data-table>
+
+    <v-schema :schema="response.type" />
+
+    <!--v-example :example="response.example" /-->
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import { Response } from './types'
+import { DataTableHeadersItem } from '../../vuetify-types'
+import { checkbox } from '../../utils'
+import VSchema from './Schema.vue'
+import VExample from './Example.vue'
 
-@Component
+@Component({
+  components:{
+  VSchema,
+  VExample
+  }
+  })
 export default class VResponse extends Vue {
   @Prop()
-  resoponse?: Response
+  response?: Response
+
+  private paramsHeaders: Array<DataTableHeadersItem> = [
+    {
+      locale: 'viewer.api.header-name',
+      value: 'name',
+      align: 'left',
+      width: '3rem'
+    },
+    {
+      locale: 'viewer.api.header-required',
+      value: 'required',
+      width: '3rem'
+    },
+    {
+      locale: 'viewer.api.header-summary',
+      sortable: false,
+      value: 'summary'
+    }
+  ]
+
+  /**
+   * 是否存在报头内容
+   */
+  private get hasHeaders(): boolean {
+    return !!this.response &&
+      !!this.response.headers &&
+      this.response.headers.length > 0
+  }
+
+  private checkbox(check: boolean): string {
+    return checkbox(check)
+  }
 }
 </script>
--- src/views/viewer/Schema.vue
+++ src/views/viewer/Schema.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>
-    <code>{{schema}}</code>
-  </div>
+  <v-tabs right>
+    <v-tab>schema</v-tab>
+    <v-tab>list</v-tab>
+
+    <v-tab-item>
+      <code class="code overflow-y-hidden pr-1 pl-1 pt-1 pb-1">{{schema}}</code>
+    </v-tab-item>
+
+    <v-tab-item>
+      {{schema}}
+    </v-tab-item>
+  </v-tabs>
 </template>
+
+<style scoped>
+.code {
+  box-shadow: none;
+  width: 100%
+}
+</style>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
