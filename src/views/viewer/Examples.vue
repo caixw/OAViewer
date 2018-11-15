869f1ec131b1771eb2cc3348f7f8fb4813bfed94
caixw <xnotepad@gmail.com>
2018-11-16 00:01:41 +0800

    调整 example

--- src/views/viewer/Examples.vue
+++ src/views/viewer/Examples.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <v-toolbar>
+    <v-toolbar flat dense>
       <v-toolbar-title v-t="'viewer.api.examples'" />
       <v-spacer></v-spacer>
-      <v-btn-toggle v-model="type">
-        <v-btn flat v-for="(exp, index) of examples" :key="index" :value="exp.mimetype">{{exp.mimetype}}</v-btn>
+      <v-btn-toggle v-model="type" v-if="showSwitcher">
+        <v-btn flat v-for="(exp, index) of examples" :key="index" :value="exp.mimetype">{{index}}/{{exp.mimetype}}</v-btn>
       </v-btn-toggle>
     </v-toolbar>
-    <v-card v-for="(exp, index) of examples" :key="index" v-show="state=exp.mimetype">
+    <v-card v-for="(exp, index) of examples" :key="index"
+    v-show="state=exp.mimetype"
+    class="pl-2 pr-2 pt-2 pb-2">
       <p>{{exp.summary}}</p>
       <code class="code">{{exp.value}}</code>
     </v-card>
@@ -31,5 +33,12 @@ export default class VExamples extends Vue {
   examples?: Example[] // NOTE: 数组属性必须为空值，否则会提示出错
 
   private type: string = '*'
+
+  /**
+   * 是否存在多条示例代码。
+   */
+  private get showSwitcher(): boolean {
+    return !!this.examples && this.examples.length > 1
+  }
 }
 </script>
